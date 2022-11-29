8a4e1f5513daa2a278f6104951578868010478d2
godwinerons <111974123+godwinerons@users.noreply.github.com>
2022-11-29 13:38:49 +0100

    done

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for x in range(10):
-    for y in range(10):
-        if x < y:
-            if (x * 10 + y) != 89:
-                print("{}{},".format(x, y), end=" ")
-            else:
-                print("{}{}".format(x, y))
+for number in range(100):
+    if int(number / 10) != number % 10 and int(number / 10) < number % 10:
+        print("{}{}".format(int(number / 10), number % 10), end="")
+        if (number != 89):
+            print(", ", end="")
+print("")
